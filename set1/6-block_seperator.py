72b20e60db48b1d7a36bd6cf7693858acffb0bfb
margany <michael.argany@gmail.com>
2019-04-08 23:04:31 -0400

    Fixed existing bugs

--- set1/6-block_seperator.py
+++ set1/6-block_seperator.py
@@ -5,25 +5,19 @@ import sys
 import base64
 
 if len(sys.argv) != 3:
-	print "Usage python %s [base64 string] [number of keys]" % sys.argv[0]
+	print "Usage: python %s [hex string] [number of keys]" % sys.argv[0]
 	exit(1)
 
-def b64_to_bin(data):
-        return ''.join([format(ord(c), '08b') for c in base64.b64decode(data)])
-
-
-bin1 = b64_to_bin(sys.argv[1])
+hexstr = sys.argv[1]
 n_keys = int(sys.argv[2])
 out = [""] * n_keys
-pos = 0
 
-print bin1
+if len(hexstr) % 2 != 0:
+	print "Error: hex string is of odd length"
+	exit(1)
 
-for i in range(0, len(bin1), 8):
-	out[pos%n_keys] += bin1[i:i+8]
-	pos+=1
+for i in range(0, len(hexstr), 2):
+	out[i/2%n_keys] += hexstr[i] + hexstr[i+1]
 
-print out
-
-for i in range(len(out)):
-	print str(i) + ": " + hex(int(out[i], 2))
+for j in range(len(out)):
+	print str(j) + ": 0x" + out[j]
--- set1/6-xor_brute.py
+++ set1/6-xor_brute.py
@@ -4,7 +4,8 @@ import sys
 import base64
 
 if len(sys.argv) != 2:
-	print "Please provide hex text"
+	print "Usage: python %s [hexstring]"
+	print "! No leading 0x or \\x in hexstring"
 	exit(1)
 
 # Check if bin_val contains unprintable ASCII
@@ -27,15 +28,15 @@ def repeat_xor(bin, c):
 	return output
 
 # Convert hex to binary, fill to make sure leading zeros aren't removed
-bin_val=bin(int(sys.argv[1],16))[2:].zfill(len(sys.argv[1]) * 4)
+bin_val=bin(int(sys.argv[1].lstrip("0x"),16))[2:].zfill(len(sys.argv[1]) * 4)
 ascii_txts=[]
 keys=[]
 for i in range(256):
 	c = '{0:08b}'.format(i)
 	out = repeat_xor(bin_val,c)
 
-	if unprintable(out):
-		continue
+#	if unprintable(out):
+#		continue
 
 	ascii=""
 	for j in range(0, len(out), 8):
@@ -58,5 +59,5 @@ for i in range(0, len(ascii_txts)):
 		if j in m:
 			sum += m[j]
 
-	if sum >= int(len(ascii_txts[i])*0.4):
+	if sum >= int(len(ascii_txts[i])*0.2):
 		print "key:" + str(keys[i]) + " txt:" + ascii_txts[i]
