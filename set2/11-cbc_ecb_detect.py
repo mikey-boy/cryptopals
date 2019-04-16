72ab6cd469999c57534fa23fbd1116ad32cc9173
mikey-boy <michael.argany@gmail.com>
2019-04-15 23:05:13 -0400

    Challenge 11 completed

--- set2/11-cbc_ecb_detect.py
+++ set2/11-cbc_ecb_detect.py
@@ -5,9 +5,9 @@ from Crypto.Random import get_random_bytes
 
 def pad(plaintext):
 	padding = 1
-	while (len(plaintext) + padding) % AES.block_size != 0:
+	while (len(plaintext) + padding) % 16 != 0:
 		padding += 1
-	plaintext += ('\\x%02x' % padding) * padding
+	plaintext += chr(padding) * padding
 	return plaintext
 
 def encrypt(plaintext, mode):
@@ -16,24 +16,32 @@ def encrypt(plaintext, mode):
 	print "Mode: %s, Prepend: %d bytes, Append: %d bytes" %(mode, prepend, append)
 
 	plaintext = get_random_bytes(prepend) + plaintext + get_random_bytes(append)
+	plaintext = pad(plaintext)
 	key = get_random_bytes(16)
 	cipher = AES.new(key, mode)
         ciphertext = cipher.encrypt(plaintext)
 	return ciphertext
 
 def detection_oracle(ciphertext):
-
+	block_set = set()
+	for i in range(0, len(ciphertext), 16):
+		if (ciphertext[i:i+16] in block_set):
+			return "ECB mode detected"
+		else:
+			block_set.add(ciphertext[i:i+16])
+	return "CBC mode detected"
 
 def main():
-	plaintext = 'Hmm aint that interesting there might be a pad'
+	# Interesting plaintext, will detect ECB if len(prepend) >= 9
+	plaintext = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 	enc = None
 
 	for i in range(10):
 		if (random.randint(0,1) == 0):
 			enc = encrypt(plaintext, AES.MODE_ECB)
-		else: 
+		else:
 			enc = encrypt(plaintext, AES.MODE_CBC)
-		detection_oracle(enc)		
+		print detection_oracle(enc)
 
 if __name__ == '__main__':
 	main()
